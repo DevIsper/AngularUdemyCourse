a755977c3c474231d115cbeae784daa94f449ce1
Gabriel Isper <2411600431@fema.edu.br>
2024-12-18 19:09:05 -0300

    feat: a lot of feats and useless folders to have as parameter for later apps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./core/components/header/header.component";
+import { UserComponent } from './core/components/user/user.component';
+import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'essentials';
+  users = DUMMY_USERS;
 }
